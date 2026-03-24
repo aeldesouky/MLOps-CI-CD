869b58f5f7ec7b4ecd659f50bfea809538fb2030
unknown <s-ahmed.eldesouky@zewailcity.edu.eg>
2026-03-24 06:52:47 +0200

    deploy job fix

--- train.py
+++ train.py
@@ -135,4 +135,6 @@ with mlflow.start_run() as run:
     print("Training complete")
 
     with open("model_info.txt", "w") as f:
-        f.write(run_id)
+        f.write(mlflow.active_run().info.run_id)
+    # with open("model_info.txt", "w") as f:
+    #     f.write(run_id)
